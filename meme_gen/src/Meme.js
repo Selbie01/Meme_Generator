3cef53113d1b3f59935bf1ff86579fea89142a14
Sawyer Selby <sawyerselby@gmail.com>
2022-08-13 21:33:15 -0500

    added text that changes every keypress using inputs even.target

--- meme_gen/src/Meme.js
+++ meme_gen/src/Meme.js
@@ -20,14 +20,43 @@ export default function Meme(){
         }))
     }
 
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
     return (
         <main>
             <div className = "form">
-                <input type ="text" placeholder = "Top text"className = "form-input"/>
-                <input type = "text" placeholder= "Bottom text" className="form-input" />
+                <input 
+                    type ="text" 
+                    placeholder = "Top text" 
+                    className = "form-input" 
+                    name = "topText" 
+                    value = {meme.topText} 
+                    onChange = {handleChange}
+                    />
+                <input 
+                    type = "text" 
+                    placeholder= "Bottom text" 
+                    className="form-input" 
+                    name = "bottomText"
+                    value = {meme.bottomText} 
+                    onChange = {handleChange}
+                />
                 <button className= "form-btn" onClick = {getMemeImage}>Get a new meme!</button>
             </div>
+            <div className='meme'>
             <img src = {meme.randomImage} className = "memeImage" alt = ""/>
+            <h2 className='meme-text top'>{meme.topText}</h2>
+            <h2 className = "meme-text bottom">{meme.bottomText}</h2>
+            </div>
+            
         </main>
     );
 }
